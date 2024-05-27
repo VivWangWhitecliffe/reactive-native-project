a1cf9b6310e4c9869ecf0e44257bd6f2681d2ea3
VivWangWhitecliffe <117702180+VivWangWhitecliffe@users.noreply.github.com>
2024-05-27 13:57:50 +1200

    Week 4

--- demoapp/App.js
+++ demoapp/App.js
@@ -1,15 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window'); // Get screen dimensions
+const HomeScreen = () => {
+  const handlePress = () => {
+    // Handle button press
+    alert('I am a cute little button!');
+  };
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button title="Press Me" onPress={handlePress} />
+      <Image source={require('./assets/photo1.jpg')} style={styles.image} />
+      
+    </View>
+  );
+};
+
+const DetailsScreen = () => {
+  return (
+    <View>
+      <Text>Details Screen</Text>
+    </View>
+  );
+};
+
+
+const Tab = createBottomTabNavigator();
+
+const AppNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Details" component={DetailsScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer> 
       <StatusBar style="auto" />
-      <AppNavigator />
     </View>
   );
 }
@@ -18,21 +54,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  screenContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: screenWidth, // Set width to screen width
+    height: screenHeight / 2, // Set height to half of screen height
+ 
+  },
+    
+
 });
 
-
-const Stack = createStackNavigator();
-
-const AppNavigator = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
